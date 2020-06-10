833a0bdb24751c25aa00724537720abcce6e72b9
MacBook Pro <sber8640@gmail.com>
2020-06-10 15:33:06 +0300

    Добавлена библиотека шагового двигателя

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,11 +5,33 @@
 #include <OneWire.h>
 #include <WiFiUdp.h>
 
+#include "BasicStepperDriver.h"
 #include "DS3231.h"  //Время
 #include "RtcDS3231.h"
 #include "TimeAlarms.h"
 #include "uEEPROMLib.h"
 #include "uptime_formatter.h"
+
+// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#define MOTOR_STEPS 200
+#define RPM 120
+
+// Since microstepping is set externally, make sure this matches the selected mode
+// If it doesn't, the motor will move at a different RPM than chosen
+// 1=full step, 2=half step etc.
+#define MICROSTEPS 1
+
+// All the wires needed for full functionality
+#define DIR 8
+#define STEP 9
+// Uncomment line to use enable/disable functionality
+//#define SLEEP 13
+
+// 2-wire basic config, microstepping is hardwired on the driver
+BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP);
+
+// Uncomment line to use enable/disable functionality
+// BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP, SLEEP);
 
 #define FIREBASE_HOST "aqua-3006a.firebaseio.com"
 #define FIREBASE_AUTH "eRxKqsNsandXnfrDtd3wjGMHMc05nUeo5yeKmuni"
@@ -668,6 +690,7 @@ void setup() {
     }
     startMainTimers();
     Timer5Min();
+    stepper.begin(RPM, MICROSTEPS);
 }
 void prWiFiStatus(int s) {
 #define VALCASE(x)                    \
@@ -697,5 +720,22 @@ void loop() {
     if (currentMillis - previousMillis > 60000UL) {
         previousMillis = currentMillis;
         // prWiFiStatus(WiFi.status());
+        // energize coils - the motor will hold position
+        // stepper.enable();
+
+        /*
+         * Moving motor one full revolution using the degree notation
+         */
+        stepper.rotate(360);
+
+        /*
+         * Moving motor to original position using steps
+         */
+        stepper.move(-MOTOR_STEPS * MICROSTEPS);
+
+        // pause and allow the motor to be moved by hand
+        // stepper.disable();
+
+        // delay(5000);
     }
 }
